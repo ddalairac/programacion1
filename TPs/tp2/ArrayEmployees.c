45947ca22e32d2e2daa0ef1d5486ee47adee89df
Diego <stereotipoarg@gmail.com>
2019-04-09 16:22:20 -0700

    base

--- TPs/tp2/ArrayEmployees.c
+++ TPs/tp2/ArrayEmployees.c
@@ -1,3 +1,19 @@
+#define NULL 0
+#define TRUE 1
+#define FALSE 0
+#define UP 1
+#define DOWN 0
+
+
+typedef struct
+{
+ int id;
+ char name[51];
+ char lastName[51];
+ float salary;
+ int sector;
+ int isEmpty;
+} Employee;
 
 
 int displayMenu(){
@@ -15,11 +31,11 @@ int displayMenu(){
     printf("2. MODIFICAR \n");
     printf("3. BAJA \n");
     printf("4. INFORMAR \n");
+    printf("5. SALIR \n");
     option = inputInt("Seleccione una opcion");
     printf("\n");
     return option;
 }
-
 
 int inputInt(char message[]){
     int number;
@@ -27,3 +43,51 @@ int inputInt(char message[]){
     scanf("%d", &number);
     return number;
 }
+
+
+//r = initEmployees(arrayEmployees, ELEMENTS);
+int initEmployees(Employee* list, int len){
+    for(int i=0; i<len;i++){
+        list[i].isEmpty = TRUE;
+    }
+ return 0;
+ /** \return int Return (-1) if Error [Invalid length or NULL pointer] - (0) if Ok*/
+}
+// r = addEmployee(arrayEmployees, ELEMENTS,id,name,lastName,salary,sector);
+int addEmployee(Employee* list, int len, int id, char name[],char lastName[],float salary,int sector){
+ return -1;
+}
+// int index = findEmployeeById(arrayEmployees, ELEMENTS,9);
+int findEmployeeById(Employee* list, int len,int id){
+ return NULL;
+}
+// r = removeEmployee(arrayEmployees, ELEMENTS,20);
+int removeEmployee(Employee* list, int len, int id){
+ return -1;
+}
+// r = sortEmployeeByName(arrayEmployees, ELEMENTS, 1);
+int sortEmployees(Employee* list, int len, int order){
+ return 0;
+}
+// r = printEmployees(arrayEmployees, ELEMENTS);
+int printEmployees(Employee* list, int length){
+    printf("%10s %10s %10s %10s %10s %10s \n", "id", "name", "lastName", "salary", "sector", "isEmpty");
+    for(int i=0; i<length; i++){
+        printf("%10d %10s %10s %10.2f %10d %10d \n", list[i].id, list[i].name, list[i].lastName, list[i].salary, list[i].sector, list[i].isEmpty);
+    }
+    /** Para que retorna un int???? */
+ return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
--- TPs/tp2/main.c
+++ TPs/tp2/main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "ArrayEmployees.h"
-
+#define T 1000
+//#define NULL 0
+#define TRUE 1
+#define FALSE 0
+//#define UP 1
+//#define DOWN 0
 /** Trabajo Práctico número 2
  *  Programación I – Laboratorio I.
  *  Tecnicatura Superior en Programación.
@@ -10,20 +16,35 @@
  *
  */
 
+
+
 int main()
 {
-    int option = displayMenu();
+    Employee employees[T];
+    initEmployees(employees, T);
+    int loop = TRUE;
 
-    if(option == 1){
-        //Se debe permitir ingresar un empleado calculando automáticamente el número de Id. El resto de los campos se le pedirá al usuario.
-    } else if(option == 2){
-        // Se ingresará el Número de Id, permitiendo modificar: o Nombre o Apellido o Salario o Sector
-    } else if(option == 3){
-        // Se ingresará el Número de Id y se eliminará el empleado del sistema.
-    } else if(option == 4){
-        printf("   INFORMAR:\n");
-        printf("\t 1. Listado de los empleados ordenados alfabeticamente por Apellido y Sector. \n");
-        printf("\t 2. Total y promedio de los salarios, y cuantos empleados superan el salario promedio. \n");
-    }
+    do{
+        int option = displayMenu();
+
+        if(option == 1){
+            //Se debe permitir ingresar un empleado calculando automáticamente el número de Id. El resto de los campos se le pedirá al usuario.
+        } else if(option == 2){
+            // Se ingresará el Número de Id, permitiendo modificar: o Nombre o Apellido o Salario o Sector
+        } else if(option == 3){
+            // Se ingresará el Número de Id y se eliminará el empleado del sistema.
+        } else if(option == 4){
+            printf("   INFORMAR:\n");
+            printf("\t 1. Listado de los empleados ordenados alfabeticamente por Apellido y Sector. \n");
+            printf("\t 2. Total y promedio de los salarios, y cuantos empleados superan el salario promedio. \n");
+
+            printEmployees(employees, 10);
+
+        } else if(option == 5){
+            loop = FALSE;
+        }
+        system("pause");
+    } while(loop);
+
     return 0;
 }
