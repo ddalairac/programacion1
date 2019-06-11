229b1a674da8179ee0a9e41bd14bb6b02b4691e8
alumno <alumno>
2019-06-10 21:08:50 -0300

    clase 23

--- TPs/tp4/src/LinkedList.c
+++ TPs/tp4/src/LinkedList.c
@@ -208,80 +208,22 @@ int ll_set(LinkedList* this, int index,void* pElement){
 int ll_remove(LinkedList* this,int index){
     int returnAux = -1;
 
-//    Node* node;
-//    Node* nextNode;
-//    Node* prevNode;
-
     Node* prevNode;
     Node* pDelNode;
 
     if(this != NULL && index >= 0 && index < this->size && this->size > 0){
-
         pDelNode = getNode(this, index);
 
         if(index == 0){
-            this->pFirstNode = pDelNode->pElement;
-
+            this->pFirstNode = pDelNode->pNextNode;
         } else {
             prevNode = getNode(this, index -1);
-            prevNode->pElement = pDelNode->pElement;
+            prevNode->pNextNode = pDelNode->pNextNode;
         }
         free(pDelNode);
         this->size--;
         returnAux = 0;
     }
-
-//       if(index == 0 && this->size > 2){
-//            /**
-//                Si el nodo a eliminar es el primero y la lista tiene mas de 2 elementos,
-//                el elemento 1 tomara la posicion del elemento 0, y se reducira el tamaño.
-//            **/
-//                node = getNode(this, 1);
-//                this->pFirstNode->pNextNode = node->pNextNode;
-//                this->pFirstNode->pElement = node->pElement;
-//                this->size--;
-//                returnAux = 0;
-//       }else{
-//           if(index==0 && this->size==1){
-//                /**
-//                    Si el nodo a eliminar es el primero y la ll tiene tamaño 1,
-//                    el pFirstNode de la ll es el eliminado, y se reduce el tamaño
-//                    de la lista
-//                **/
-//                this->pFirstNode=NULL;
-//                this->size--;
-//                returnAux = 0;
-//           }else{
-//               /**
-//                    Si el elemento a eliminar es el siguiente al pFirstNode,
-//                    se establece al nodo en el indice 2 como pNextNode de
-//                    pFirstNode, y se reduce el tamaño de la lista.
-//               **/
-//                if(index==1){
-//                    node=getNode(this, index);
-//                    nextNode=getNode(this, index+1);
-//                    this->pFirstNode->pNextNode=nextNode;
-//                    this->size--;
-//                    node=NULL;
-//                    returnAux = 0;
-//               }else{
-//                   /**
-//                        Si el elemento a eliminar es cualquier otro,
-//                        el nodo anterior al deseado apuntara al siguiente,
-//                        y se seteara como NULL al nodo a eliminar.
-//                        Se recude el tamaño de la lista en 1.
-//                   **/
-//                    node=getNode(this, index);
-//                    nextNode=getNode(this, index+1);
-//                    prevNode=getNode(this, index-1);
-//                    prevNode->pNextNode=nextNode;
-//                    this->size--;
-//                    node=NULL;
-//                    returnAux = 0;
-//               }
-//           }
-//        }
-//    }
 
     return returnAux;
 }
@@ -294,10 +236,29 @@ int ll_remove(LinkedList* this,int index){
                         ( 0) Si funciono correctamente
  *
  */
-int ll_clear(LinkedList* this)
-{
+int ll_clear(LinkedList* this){
     int returnAux = -1;
+    int i;
+    Node* pDelNode;
+    Node* pAuxNode;
 
+    if(this != NULL && this->size > 0){
+
+        for(i = 0; i < this->size; i++){
+            pDelNode=getNode(this,i);
+            if(pDelNode!=NULL){
+                pAuxNode = pDelNode->pNextNode;
+                this->size--;
+                free(pDelNode);
+            }
+            free(pAuxNode);
+        }
+        this->pFirstNode = NULL;
+
+        if(this->size == 0){
+            returnAux = 0;
+        }
+    }
     return returnAux;
 }
 
@@ -309,10 +270,13 @@ int ll_clear(LinkedList* this)
                         ( 0) Si funciono correctamente
  *
  */
-int ll_deleteLinkedList(LinkedList* this)
-{
+int ll_deleteLinkedList(LinkedList* this){
     int returnAux = -1;
-
+    if(this != NULL){
+        ll_clear(this);
+        free(this);
+        returnAux = 0;
+    }
     return returnAux;
 }
 
